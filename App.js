d488f042ae9f7ce6186a36e82ebacc50b77d190d
Abdullah Abdul Majeed <Abo-Zahra1991@hotmail.com>
2021-04-18 14:29:39 +0800

    Update version
    dubug

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-
 /* 
 TITLE: An Event Management System
 PARTICIPANTS: Group B
@@ -10,7 +9,6 @@ year.
 4. First comes, first served basis.
 5. The event is operating from 8am until 5pm.
 */
-
 
 /*Use the readline interface to add communication with the users 
 const readline = require('readline');
@@ -37,80 +35,133 @@ output: process.stdout
 //before run this page , add antd first 
 //npm install antd --save  or   yarn add antd
 */
-import React, { Component, Fragment, Table } from 'react';
-import 'antd/dist/antd.css';
-import { Input,Button,List,Calendar,Select,DatePicker,TimePicker } from 'antd';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component, Fragment, Table } from "react";
+import "antd/dist/antd.css";
+import {
+  Input,
+  Button,
+  List,
+  Calendar,
+  Select,
+  DatePicker,
+  TimePicker,
+} from "antd";
+import { StyleSheet, Text, View } from "react-native";
 
-import eventObject from './eventObject.js'
+// import eventObject from "./eventObject.js";
 // REQUIRED VARIABLES
 //var eventObject = require('eventObject'); // THIS LINE OF CODE IS ENABLING US TO USE WHATEVER WE WROTE INSIDE OF THE eventObject.js HERE.
 /**
- * FOR EXAMPLE, COPY THE FOLLOWING LINE OF CODE TO CREATE A NEW EVENT OBJECT >>>>> var EVENT = new EVENT; 
+ * FOR EXAMPLE, COPY THE FOLLOWING LINE OF CODE TO CREATE A NEW EVENT OBJECT >>>>> var EVENT = new EVENT;
  */
 
- export default class App extends React.Component { 
-
-  constructor(EVENT){
-    super(); 
+export default class App extends React.Component {
+  constructor(EVENT) {
+    super();
     this.EVENT = {
-     eventName: "Some events, try to have array",
-     Date: Date( 'month', 'day', 'hours', 'minutes' ),
-     eventVenue: ["0"],
-     operatingStartTime: Date( 'hours', 'minutes'  ),
-     operatingEndTime: Date( 'hours' , 'minutes'  )
-    } 
-    }
- 
-   AddDate(){
-     Date = new Date( month, day, hours, minutes);
-     this.setEVENT({Date: Date }); //click adds new date 
-   }
-   
-   render() {
-   return (
-   
-   <View style={styles.container}>
-   <Text style={styles.header}>React Event Manager</Text>
-   <eventObject />
-   <Text style={styles.contents}>The event name is: {this.EVENT.eventName}</Text> 
- 
-   <Text style={styles.contents}onPress={() => this.AddDate()}>
-    Add New Date: {this.EVENT.Date} </Text>
-    <Text style={styles.contents}>The event venue is: {this.EVENT.eventVenue}</Text> 
-    <Text style={styles.contents}>The event starting time is: {this.EVENT.operatingStartTime}</Text> 
-    <Text style={styles.contents}>The event ending time is: {this.EVENT.operatingEndTime}</Text> 
-   </View>
-   ); 
-   }
+      eventName: "Some events, try to have array",
+      Date: Date("month", "day", "hours", "minutes"),
+      eventVenue: ["0"],
+      operatingStartTime: Date("hours", "minutes"),
+      operatingEndTime: Date("hours", "minutes"),
+    };
   }
 
- 
-  const styles = StyleSheet.create({ 
-   container: {
-     zIndex: 3,
-   justifyContent: 'flex-start', 
-   alignItems: 'center', 
-   backgroundColor: 'skyblue',
-   }, 
-   
-   header: {
-   flex: 1,
-   zIndex: 1,
-   marginTop: 60,
-   fontSize: 60, 
-   textAlign: 'center', 
-   backgroundColor: 'blue',
-   margin: 10,
-   },
-   zIndex: 2,
-   contents: {
-   textAlign: 'center', 
-   color: '#333333', 
-   marginBottom: 5,
-   } 
-  })
+  AddDate() {
+    var date = new Date("month", "day", "hours", "minutes");
+    this.EVENT.Date = date;
+    this.EVENT.eventName = "new event nameing";
+  }
 
+  render() {
+    const eventObjectView = () => {
+      return (
+        <table>
+          <h1>Simple table to view the event {this.EVENT} object:</h1>
+          <tr>
+            <th>Attribute:</th>
+            <th>Information:</th>
+          </tr>
+          <tr>
+            <td>Event Name:</td>
+            <td>
+              <div>{this.EVENT.eventName}</div>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+            </td>
+          </tr>
+          <tr>
+            <td>Event Venue:</td>
+            <td>
+              <div>{this.EVENT.eventVenue}</div>
+            </td>
+          </tr>
+          <tr>
+            <td>Event Date:</td>
+            <td>
+              <div>{this.EVENT.Date}</div>
+            </td>
+          </tr>
+          <tr>
+            <td>Starting Time:</td>
+            <td>
+              <div>{this.EVENT.operatingStartTime}</div>
+            </td>
+          </tr>
+          <tr>
+            <td>Ending Time:</td>
+            <td>
+              <div>{this.EVENT.operatingEndTime}</div>
+            </td>
+          </tr>
+        </table>
+      );
+    };
 
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>React Event Manager</Text>
+        <eventObjectView />
+        <Text style={styles.contents}>
+          The event name is: {this.EVENT.eventName}
+        </Text>
 
+        <Text style={styles.contents} onPress={() => this.AddDate()}>
+          Add New Date: {this.EVENT.Date}{" "}
+        </Text>
+        <Text style={styles.contents}>
+          The event venue is: {this.EVENT.eventVenue}
+        </Text>
+        <Text style={styles.contents}>
+          The event starting time is: {this.EVENT.operatingStartTime}
+        </Text>
+        <Text style={styles.contents}>
+          The event ending time is: {this.EVENT.operatingEndTime}
+        </Text>
+      </View>
+    );
+  }
+}
 
+const styles = StyleSheet.create({
+  container: {
+    zIndex: 3,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: "skyblue",
+  },
+
+  header: {
+    flex: 1,
+    zIndex: 1,
+    marginTop: 60,
+    fontSize: 60,
+    textAlign: "center",
+    backgroundColor: "blue",
+    margin: 10,
+  },
+  zIndex: 2,
+  contents: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5,
+  },
+});
--- eventObject.js
+++ eventObject.js
@@ -10,108 +10,134 @@ year. (ATTAINED USING THE DATE CONSTRUCTOR)
 5. The event is operating from 8am until 5pm. (RECHECK)
 */
 
-// REQUIRED VARIABLES 
+// REQUIRED VARIABLES
 /**
- * WE SUPPOSEDLY GOING TO DECLARE ONE VARIABLE WITH THE NAME 'id', FOR INTERNAL MONGODB USE IF NEEDED 
+ * WE SUPPOSEDLY GOING TO DECLARE ONE VARIABLE WITH THE NAME 'id', FOR INTERNAL MONGODB USE IF NEEDED
  */
 
-
-
+import React, { Component, Fragment, Table } from "react";
 
 let EVENT; // IT SHOULD REPRESENT A FULL DAY EVENT
 var eventName; // HAVE A UNIQUE NAME FOR EACH EVENT OBJECT
-const eventVenue = ['KICT MAIN HALL', 'ECON MAIN HALL', 'LAW MAIN COURT'];
+const eventVenue = ["KICT MAIN HALL", "ECON MAIN HALL", "LAW MAIN COURT"];
 
- // SPECIFYING FEW UNIQUE eventVenue ATTRIBUTE VALUES, TO STATE WHERE THE EVENT IS TAKING ACTION
+// SPECIFYING FEW UNIQUE eventVenue ATTRIBUTE VALUES, TO STATE WHERE THE EVENT IS TAKING ACTION
 /*
-* MAKING USE OF THE JS UTILITY PROVIDED, THE DATE CONSTRUCTOR
-* USUALLY IS IN THE FOLLOWING FORM >>> var Date = new Date();
-*/ 
-var Date; 
-var operatingStartTime; // THE STARTING POINT OF ANY EVENT 
+ * MAKING USE OF THE JS UTILITY PROVIDED, THE DATE CONSTRUCTOR
+ * USUALLY IS IN THE FOLLOWING FORM >>> var Date = new Date();
+ */
+var Date;
+var operatingStartTime; // THE STARTING POINT OF ANY EVENT
 var operatingEndTime; // THE ENDING POINT OF ANY EVENT
 
-class eventObject {
-    
-    constructor(){// DEFAULT CONSTRUCTOR
-        /**
-         * THE MAIN USAGE OF THE DEFAULT CONSTRUCTOR IS TO INITIALIZE THE SPECIFIC REQUIRED VALUE TYPES 
-         * FOR ALL PREVIOUSLY DECLARED VARIABLES EACH BASED ON THEIR OWN UTILITY PURPOSES
-         */
-        this.EVENT = {
-            eventName: "",
-            Date: Date( month, day, hours, minutes ),
-            eventVenue: [0],
-            operatingStartTime: Date( hours, minutes ),
-            operatingEndTime: Date( hours , minutes )
-        }
-    }
+class eventObject extends React.Component {
+  constructor() {
+    // DEFAULT CONSTRUCTOR
+    /**
+     * THE MAIN USAGE OF THE DEFAULT CONSTRUCTOR IS TO INITIALIZE THE SPECIFIC REQUIRED VALUE TYPES
+     * FOR ALL PREVIOUSLY DECLARED VARIABLES EACH BASED ON THEIR OWN UTILITY PURPOSES
+     */
+    this.EVENT = {
+      eventName: "",
+      Date: Date(month, day, hours, minutes),
+      eventVenue: [0],
+      operatingStartTime: Date(hours, minutes),
+      operatingEndTime: Date(hours, minutes),
+    };
+    this.eventObjectView();
+  }
 
-  /*  constructor(EVENT){// OVER-RIDE THE DEFAULT CONSTRUCTOR TO INTERACT WITH THE OBJECT MODEL
-        this.EVENT = EVENT;
-    }
-*/
-    // GETTERS AND SETTERS 
-    get EVENT() { return this.EVENT; } // GETS EVENT OBJECT
-    set EVENT(EVENT) { this.EVENT = EVENT; } // SETS EVENT OBJECT
+  // constructor(EVENT){           // OVER-RIDE THE DEFAULT CONSTRUCTOR TO INTERACT WITH THE OBJECT MODEL
+  //     this.EVENT = EVENT;
+  // }
 
-    get eventName() { return this.eventName;} // GETS eventName ATTRIBUTE OF A PARTICULAR EVENT OBJECT
-    set eventName(eventName) { this.eventName = eventName; } // SETS eventName ATTRIBUTE OF A PARTICULAR EVENT OBJECT
+  // GETTERS AND SETTERS
+  get EVENT() {
+    return this.EVENT;
+  } // GETS EVENT OBJECT
+  set EVENT(EVENT) {
+    this.EVENT = EVENT;
+  } // SETS EVENT OBJECT
 
-    get eventVenue() { return this.eventVenue; } // GETS eventVenue ATTRIBUTE OF A PARTICULAR EVENT OBJECT
-    set eventVenue(eventVenue) { this.eventVenue = eventVenue; } // SETS eventVenue ATTRIBUTE OF A PARTICULAR EVENT OBJECT
+  get eventName() {
+    return this.eventName;
+  } // GETS eventName ATTRIBUTE OF A PARTICULAR EVENT OBJECT
+  set eventName(eventName) {
+    this.eventName = eventName;
+  } // SETS eventName ATTRIBUTE OF A PARTICULAR EVENT OBJECT
 
-    get Date() { return this.Date; } // GETS Date ATTRIBUTE CONSTRUCTOR FOR A PARTICULAR EVENT OBJECT
-    set Date(Date) { this.Date = Date; } // SETS Date ATTRIBUTE CONSTRUCTOR FOR A PARTICULAR EVENT OBJECT
+  get eventVenue() {
+    return this.eventVenue;
+  } // GETS eventVenue ATTRIBUTE OF A PARTICULAR EVENT OBJECT
+  set eventVenue(eventVenue) {
+    this.eventVenue = eventVenue;
+  } // SETS eventVenue ATTRIBUTE OF A PARTICULAR EVENT OBJECT
 
-    get operatingStartTime() { return this.operatingStartTime; }
-    set operatingStartTime(operatingStartTime) {this.operatingStartTime = operatingStartTime; } // PLEASE, VALIDATE AND FIX IF NECESSARY 
+  get Date() {
+    return this.Date;
+  } // GETS Date ATTRIBUTE CONSTRUCTOR FOR A PARTICULAR EVENT OBJECT
+  set Date(Date) {
+    this.Date = Date;
+  } // SETS Date ATTRIBUTE CONSTRUCTOR FOR A PARTICULAR EVENT OBJECT
 
-    get operatingEndTime() { return this.operatingEndTime; }
-    set operatingEndTime(operatingEndTime) {this.operatingEndTime = operatingEndTime; } // PLEASE, VALIDATE AND FIX IF NECESSARY   
-    
-    
+  get operatingStartTime() {
+    return this.operatingStartTime;
+  }
+  set operatingStartTime(operatingStartTime) {
+    this.operatingStartTime = operatingStartTime;
+  } // PLEASE, VALIDATE AND FIX IF NECESSARY
 
-}
+  get operatingEndTime() {
+    return this.operatingEndTime;
+  }
+  set operatingEndTime(operatingEndTime) {
+    this.operatingEndTime = operatingEndTime;
+  } // PLEASE, VALIDATE AND FIX IF NECESSARY
 
-const eventObjectView = () => {
-
-
-    return ( 
-
-<table>
-    <h1>Simple table to view the event {this.EVENT} object:</h1>
-  <tr>
-    <th>Attribute:</th>
-    <th>Information:</th>
-  </tr>
-  <tr>
-    <td>Event Name:</td>
-    <td><div>{this.EVENT.eventName}</div></td>
-  </tr>
-  <tr>
-    <td>Event Venue:</td>
-    <td><div>{this.EVENT.eventVenue}</div></td>
-  </tr>
-  <tr>
-    <td>Event Date:</td>
-    <td><div>{this.EVENT.Date}</div></td>
-  </tr>
-  <tr>
-    <td>Starting Time:</td>
-    <td><div>{this.EVENT.operatingStartTime}</div></td>
-  </tr>
-  <tr>
-    <td>Ending Time:</td>
-    <td><div>{this.EVENT.operatingEndTime}</div></td>
-  </tr>
-</table>
-
-
+  eventObjectView = () => {
+    return (
+      <table>
+        <h1>Simple table to view the event {this.EVENT} object:</h1>
+        <tr>
+          <th>Attribute:</th>
+          <th>Information:</th>
+        </tr>
+        <tr>
+          <td>Event Name:</td>
+          <td>
+            <div>{this.EVENT.eventName}</div>
+          </td>
+        </tr>
+        <tr>
+          <td>Event Venue:</td>
+          <td>
+            <div>{this.EVENT.eventVenue}</div>
+          </td>
+        </tr>
+        <tr>
+          <td>Event Date:</td>
+          <td>
+            <div>{this.EVENT.Date}</div>
+          </td>
+        </tr>
+        <tr>
+          <td>Starting Time:</td>
+          <td>
+            <div>{this.EVENT.operatingStartTime}</div>
+          </td>
+        </tr>
+        <tr>
+          <td>Ending Time:</td>
+          <td>
+            <div>{this.EVENT.operatingEndTime}</div>
+          </td>
+        </tr>
+      </table>
     );
+  };
 }
 
-export default eventObjectView;
+// export default eventObjectView;
 /* HOW TO RETRIEVE DATE DATA
 EVENT.getFullYear();      
 EVENT.getMonth();         
@@ -123,4 +149,3 @@ EVENT.getSeconds();
 EVENT.getMilliseconds();  
 EVENT.getTime();          
 */
-
